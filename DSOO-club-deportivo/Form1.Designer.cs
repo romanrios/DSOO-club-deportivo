5424855cc2e238c7f1046efc98db0c03164156f7
Román Ríos <romanrios@live.com>
2024-10-07 01:15:55 -0300

    fixes

--- DSOO-club-deportivo/Form1.Designer.cs
+++ DSOO-club-deportivo/Form1.Designer.cs
@@ -28,14 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             txtUsuario = new TextBox();
             txtPass = new TextBox();
             btnIngresar = new Button();
+            pictureBox1 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
             // txtUsuario
             // 
-            txtUsuario.Location = new Point(82, 56);
+            txtUsuario.Location = new Point(50, 140);
             txtUsuario.Name = "txtUsuario";
             txtUsuario.Size = new Size(163, 23);
             txtUsuario.TabIndex = 0;
@@ -45,7 +48,7 @@
             // 
             // txtPass
             // 
-            txtPass.Location = new Point(82, 116);
+            txtPass.Location = new Point(50, 200);
             txtPass.Name = "txtPass";
             txtPass.Size = new Size(163, 23);
             txtPass.TabIndex = 1;
@@ -55,7 +58,7 @@
             // 
             // btnIngresar
             // 
-            btnIngresar.Location = new Point(82, 183);
+            btnIngresar.Location = new Point(50, 265);
             btnIngresar.Name = "btnIngresar";
             btnIngresar.Size = new Size(163, 35);
             btnIngresar.TabIndex = 2;
@@ -63,17 +66,29 @@
             btnIngresar.UseVisualStyleBackColor = true;
             btnIngresar.Click += btnIngresar_Click;
             // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(98, 30);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(69, 74);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 3;
+            pictureBox1.TabStop = false;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(333, 268);
+            ClientSize = new Size(261, 344);
+            Controls.Add(pictureBox1);
             Controls.Add(btnIngresar);
             Controls.Add(txtPass);
             Controls.Add(txtUsuario);
             Name = "Form1";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Iniciar Sesión";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -83,5 +98,6 @@
         private TextBox txtUsuario;
         private TextBox txtPass;
         private Button btnIngresar;
+        private PictureBox pictureBox1;
     }
 }
